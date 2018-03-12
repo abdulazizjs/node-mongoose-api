424a6868854cabdb3c01ce797843ad050573e1de
Abdulaziz Yakubov <abdulazizyakubov@gmail.com>
2018-03-12 22:36:40 +0500

    mongoose-api

---
+++ server/models/todo.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+let Todo = mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
+
+module.exports = {Todo}
---
+++ server/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose-todo');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.status(400).send(doc);
+  },(e) => {
+    console.log(e);
+  })
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+})
