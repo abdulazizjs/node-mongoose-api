c9762e2631f237ce64635f3da837752fd61e85d4
Abdulaziz Yakubov <abdulazizyakubov@gmail.com>
2018-03-15 00:08:02 +0500

    Test the DELETE/todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -98,3 +98,40 @@ describe('GET/todos/:id', () => {
   })
 
 });
+
+describe('DELETE /todos/:id', () => {
+  it('Should remove a todo', (done) => {
+      var hexId = todos[1]._id.toHexString();
+      request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+         Todo.findById(hexId).then((todo) => {
+            expect(todo).toNotExist();
+            done();
+         }).catch((err) => done(err))
+      });
+  });
+
+  it('Should return 404 if todo not found', (done) => {
+    var hexId = new ObjectId().toHexString();
+
+      request(app)
+       .delete(`todos/${hexId}`)
+       .expect(404)
+       .end(done);
+  })
+
+  it('Should return 404 if object id is invalid', (done) => {
+    request(app)
+    .delete('/todos/123abc')
+    .expect(404)
+    .end(done);
+  })
+})
