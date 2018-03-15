81404ff20925f0bf95b080090bab15093801ad3b
Abdulaziz Yakubov <abdulazizyakubov@gmail.com>
2018-03-15 23:15:53 +0500

    Updating PATCH/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectId} = require('mongodb');
+const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose-todo');
 const {Todo} = require('./models/todo');
@@ -62,6 +63,33 @@ app.delete('/todos/:id', (req, res) => {
       res.send(todo);
     }).catch((e) => res.status(400).send());
 
+})
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send();
+
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+    
+  }
+
+   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+      if(!todo){
+        return res.status(404).send();
+
+      }
+        res.send({todo});
+   }).catch((e) => {
+     res.status(400).send();
+   })
 })
 
 app.listen(port, () => {
