4cd102e7f43925a3c9df238da7c229bb432fcbfd
Abdulaziz Yakubov <abdulazizyakubov@gmail.com>
2018-03-22 21:17:50 +0500

    Associate usrs with todos

--- server/models/todo.js
+++ server/models/todo.js
@@ -14,6 +14,11 @@ var Todo = mongoose.model('Todo', {
   completedAt: {
     type: Number,
     default: null
+  },
+  _creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+
   }
 });
 
--- server/server.js
+++ server/server.js
@@ -16,10 +16,14 @@ var app = express();
 var port = process.env.PORT;
 
 app.use(bodyParser.json());
-app.post('/todos', (req, res) => {
+
+
+app.post('/todos', authenticate, (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   });
+
   todo.save().then((doc) => {
     res.status(400).send(doc);
   },(e) => {
@@ -27,22 +31,25 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({todos})
   }, (e) => {
     res.status(400).send(e);
   });
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
     if(!ObjectId.isValid(id)){
       console.log('ID is not valid');
     return  res.status(404).send();
     }
-    Todo.findById({
-      _id: id
+    Todo.findByOne({
+      _id: id,
+      _creator: req.user._id
     }).then((todo) => {
       if(!todo){
         res.status(404).send();
@@ -68,7 +75,7 @@ app.delete('/todos/:id', (req, res) => {
 
 })
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
   if(!ObjectId.isValid(id)){
@@ -84,7 +91,7 @@ app.patch('/todos/:id', (req, res) => {
 
   }
 
-   Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+   Todo.findOneAndUpdate({_id: id, _creator: req.user._id}, {$set: body}, {new: true}).then((todo) => {
       if(!todo){
         return res.status(404).send();
 
