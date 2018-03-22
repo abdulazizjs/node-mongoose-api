a9b6587421f8b2e5888ed67f61a075c274a82bbe
Abdulaziz Yakubov <abdulazizyakubov@gmail.com>
2018-03-22 16:26:03 +0500

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -116,6 +116,18 @@ app.get('/users/me',authenticate, (req, res) => {
 res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+User.findByCredentials(body.email, body.password).then((user) => {
+  return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 })
